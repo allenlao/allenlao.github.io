cd7d40febc3febf5b6203558f3e4ff517aebe91d
Allen Lao <al4146@nyu.edu>
2017-02-28 00:55:16 -0500

    Add files via upload

--- contact_me.js
+++ contact_me.js
@@ -20,15 +20,14 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//formspree.io/allenlao26@gmail.com",
-                method: "POST";
+                url: "././mail/contact_me.php",
+                type: "POST",
                 data: {
                     name: name,
                     phone: phone,
                     email: email,
                     message: message
                 },
-                dataType: json,
                 cache: false,
                 success: function() {
                     // Success message
